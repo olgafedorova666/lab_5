539710f2045b78059abe59a4612c3554fae28e6f
Olga Fedorova <olgafedorova666@gmail.com>
2022-10-12 19:19:55 +0300

    POFDLKJGHJKL:LKJGHJ":

--- lab_5/__main__.py
+++ lab_5/__main__.py
@@ -1,8 +1,29 @@
+from pkg_resources import empty_provider
 from . import boot
 
-with open('adams.dd', 'rb') as file: 
+with open('adams.dd', 'rb') as file:
     parsed_boot = boot.Boot(file.read())
     print(parsed_boot.oem_name.decode('ascii'))
     print(int.from_bytes(parsed_boot.bytes_per_sector, byteorder='little'))
     print(parsed_boot.system_identifier.decode('ascii'))
     print(parsed_boot.volume_label.decode('ascii'))
+    rootDirSectors = int(int.from_bytes(parsed_boot.root_entry_count, byteorder='little')
+                         * 32) / int.from_bytes(parsed_boot.bytes_per_sector, byteorder='little')
+    dataSec = int.from_bytes(parsed_boot.total_sectors_16, byteorder='little') - (int.from_bytes(parsed_boot.reserved_sectors_count, byteorder='little') + (
+        int.from_bytes(parsed_boot.table_count, byteorder='little') * int.from_bytes(parsed_boot.table_size_16, byteorder='little')) + rootDirSectors)
+    print(dataSec)
+    root_dir_offset = int.from_bytes(parsed_boot.table_size_16, byteorder='little') * \
+        2 * int.from_bytes(parsed_boot.bytes_per_sector,
+                           byteorder='little') + 512
+    root_dir_entries_list = []
+    print(root_dir_offset+int(rootDirSectors) * int.from_bytes(parsed_boot.bytes_per_sector, byteorder='little'))
+    for i in range(0, int(rootDirSectors) * int.from_bytes(parsed_boot.bytes_per_sector, byteorder='little'), 32):
+        file.seek(root_dir_offset+i)
+        entry_bytes = file.read(32)
+        try:
+            dir_entry = boot.RootDirEntry(entry_bytes)
+            print(dir_entry.file_name)
+            root_dir_entries_list.append(dir_entry)
+        except ValueError:
+            pass
+
--- lab_5/boot.py
+++ lab_5/boot.py
@@ -1,3 +1,5 @@
+import struct
+
 class Boot:
     def __init__(self, data) -> None:
         self.booyjmp = data[0:3]
@@ -14,7 +16,7 @@ class Boot:
         self.head_side_count = data[26:28]
         self.hidden_sector_count = data[28:32]
         self.total_sectors_32 = data[32:36]
-        self.nt_flags_resrved = data[37:38]
+        self.nt_flags_resrved = data[36:38]
         self.signature = data[38:39]
         self.volume_id = data[39:43]
         self.volume_label = data[43:54]
@@ -22,5 +24,81 @@ class Boot:
         self.boot_code = data[62:510]
         self.bootable_partition_signature = data[510:512]
 
-    def __repr__(self) -> str:
+class RootDirEntry:
+    def __init__(self, entry) -> None:
+        if entry == b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00':
+            raise ValueError
+        if entry[11:12] == b'\01':
+            self.attribute = 'READ_ONLY'
+            self.parse_standart(entry)
+        elif entry[11:12] == b'\02':
+            self.attribute = 'HIDDEN'
+            self.parse_standart(entry)
+        elif entry[11:12] == b'\04':
+            self.attribute = 'SYSTEM'
+            self.parse_standart(entry)
+        elif entry[11:12] == b'\08':
+            self.attribute = 'VOLUME_ID'
+            self.parse_standart(entry)
+        elif entry[11:12] == b'\10':
+            self.attribute = 'DIRECTORY'
+            self.parse_standart(entry)
+        elif entry[11:12] == b'\x20':
+            self.attribute = 'ARCHIVE'
+            self.parse_standart(entry)
+        elif entry[11:12] == b'\x0f':
+            self.attribute = 'LONG_FILE_NAME'
+            self.parse_standart(entry)
+        else: 
+            self.attribute = f'UNKNOWN_{entry[11:12]}'
+            self.parse_standart(entry)
         pass
+
+    def parse_standart(self, entry) -> None:
+        self.file_name = entry[0:11]
+        self.nt_reserved = entry[12:13]
+        self.creation_time_s = entry[13:14]
+        self.creation_time_raw = entry[14:16]
+        self.creation_date_raw = entry[16:18]
+        self.last_access_date_raw = entry[18:20]
+        self.first_cluster_high = entry[20:22]
+        self.modification_time_raw = entry[22:24]
+        self.modification_date_raw = entry[24:26]
+        self.first_cluster_low = entry[26:28]
+        self.file_size = entry[28:32]
+        
+        self.creation_time = self.decode_fat_time(self.creation_time_raw
+        )
+        self.creation_date = self.decode_fat_date(self.creation_date_raw
+        )
+        self.last_access_date = self.decode_fat_date(self.last_access_date_raw)
+        self.modification_time = self.decode_fat_time(self.modification_time_raw)
+        self.modification_date = self.decode_fat_date(self.modification_date_raw
+        )
+
+
+    def as_unsigned(self, bs, endian='<'):
+        unsigned_format = {1: 'B', 2: 'H', 4: 'L', 8: 'Q'}
+        if len(bs) <= 0 or len(bs) > 8:
+            raise ValueError()
+        fill = '\x00'
+        while len(bs) not in unsigned_format:
+            bs = bs + fill
+        result = struct.unpack(endian + unsigned_format[len(bs)], bs)[0]
+        return result
+
+    def decode_fat_time(self, time_bytes, tenths=0, tz='EDT'):
+        v = self.as_unsigned(time_bytes)
+        second = int(int(0x1F & v) * 2)
+        if tenths > 100:
+            second += 1
+        minute = (0x7E0 & v) >> 5
+        hour = (0xF800 & v) >> 11
+        return '{:02}:{:02}:{:02} ({})'.format(hour, minute, second, tz)
+
+    def decode_fat_date(self, date_bytes):
+        v = self.as_unsigned(date_bytes)
+        day = 0x1F & v
+        month = (0x1E0 & v) >> 5
+        year = ((0xFE00 & v) >> 9) + 1980
+        return '{}-{:02}-{:02}'.format(year, month, day)
